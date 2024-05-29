f267619e0db80859a38b3c142a9ab49e4881b377
Esther299 <esthergc_2@hotmail.com>
2024-05-29 15:07:26 +0200

    Deleted a comment by its id

--- routers/api-router.js
+++ routers/api-router.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const topicsRouter = require('./topics-router');
 const articlesRouter = require('./articles-router')
+const commentsRouter = require('./comments-router')
 const { getEndpoints } = require('../controllers/api-controller');
 
 const apiRouter = express.Router();
@@ -8,5 +9,6 @@ const apiRouter = express.Router();
 apiRouter.use('/topics', topicsRouter);
 apiRouter.get('/', getEndpoints);
 apiRouter.use('/articles', articlesRouter);
+apiRouter.use('/comments', commentsRouter);
 
 module.exports = apiRouter;
